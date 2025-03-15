380717b8ad485f1ce9a7f67d06ba2ce825ad0295
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2025-03-15 14:13:59 +0100

    build: sonar

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ export default {
   ...presets.createCjsPreset(),
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   roots: ["<rootDir>/projects/ngx-voyage"],
-  coverageReporters: ["json", "text"],
+  coverageReporters: ["json", "text", "lcov"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
 } satisfies Config;
